c85063c095894ce2960bffa56aba4106870599e1
Kishorechristy7428 <107244119+Kishorechristy7428@users.noreply.github.com>
2022-07-15 23:44:43 +0530

    stores

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return"Hello,World!"
+stores = [
+    {
+        'name': 'My wonderful Stores',
+        'items': [
+            {
+            'names': 'My items',
+            'price':15.99
+            }
+        ]
+    }
+]
+#POST-used to receive data
+#GET-used to send data back only
+
+#POST /store data:{name:}
+@app.route('/store',methods=['POST'])
+def create_store():
+    pass
+
+#GET/store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+#GET/store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+#POST/store/<string:name>/item{name:,price:}
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
 
 app.run(port=5000)
